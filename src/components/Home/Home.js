edabd27af9696d76bbddaa710b8385f7cb62dc68
Devin Encalada <devinencalada@gmail.com>
2018-07-12 13:22:21 -0400

    progress so far

---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
+
+class Home extends Component{
+    render(){
+        return(
+            <Grid>
+                <Jumbotron>
+                    <h2>
+                        Devin Encalada
+                    </h2>
+                    <p>Resume website</p>
+                </Jumbotron>
+                <Link to="/about">
+                    <Button bsStyle="primary">About</Button>
+                </Link>
+            </Grid>
+        )
+    }
+}
+
+export default Home;
