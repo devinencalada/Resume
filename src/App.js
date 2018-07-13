2dba7f6f3c820bf9dd96a19009c7e78dc80ba6c4
Devin Encalada <devinencalada@gmail.com>
2018-07-13 18:01:45 -0400

    made progress to the landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import { Grid, Image } from 'semantic-ui-react';
 import Home from './components/Home';
 import About from './components/About';
 import NavMenu from './components/NavMenu';
@@ -10,10 +9,10 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <NavMenu />
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
+          <div>
+            <NavMenu />
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
         </div>
       </Router>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
+import { Container, Header, Button, Grid } from 'semantic-ui-react';
 import './Home.css';
 
 class Home extends Component{
     render(){
         return(
-            <div>
-                <h2>
-                    Devin Encalada
-                </h2>
-                <p>Resume website</p>
-            </div>
+                <Container text>
+                    <Grid>
+                        <Grid.Row>
+                            <Header
+                                as='h1'
+                                content='John Devin Encalada'
+                                style={{
+                                    fontSize: '4em',
+                                    fontWeight: 'normal',
+                                    marginBottom: 0,
+                                    marginTop: '3em',
+                                }}
+                            />
+                            <Header
+                                as='h2'
+                                content='Software Engineer'
+                                style={{
+                                    fontSize: '1.7em',
+                                    fontWeight: 'normal',
+                                    marginTop: '1.5em',
+                                }}
+                            />
+                        </Grid.Row>
+                        <Grid.Row columns={10}>
+                            <Grid.Column>
+                                <a href="https://www.facebook.com/devin.encalada" target="_blank" rel="noopener noreferrer">
+                                    <Button circular color='facebook' icon='facebook' />
+                                </a>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <a href="https://twitter.com/devin_encalada" target="_blank" rel="noopener noreferrer">
+                                    <Button circular color='twitter' icon='twitter' />
+                                </a>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <a href="https://www.linkedin.com/in/john-encalada-b6680b146" target="_blank" rel="noopener noreferrer">
+                                    <Button circular color='linkedin' icon='linkedin' />
+                                </a>
+                            </Grid.Column>
+
+                        </Grid.Row>
+                    </Grid>
+                </Container>
         )
     }
 }
