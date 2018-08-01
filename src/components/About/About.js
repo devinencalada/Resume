6fe9ab2c4ce4af2d24604b5700d6e5228c15bc6d
Devin Encalada <devinencalada@gmail.com>
2018-08-01 16:10:54 -0400

    improved about page to stack containers on smaller mobile screens

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './About.css';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Header } from 'semantic-ui-react';
 import profilePic from '../../images/profilePic.png';
 
 import './About.css';
@@ -8,15 +8,20 @@ import './About.css';
 class About extends Component{
     render(){
         return(
-            <Grid columns="equal" divided>
-                <Grid.Row>
-                    <Grid.Column width={6}>
+            <Grid container stackable columns={2}>
+                    <Grid.Column>
                         <Image src={profilePic} size="large" circular centered/>
                     </Grid.Column>
                     <Grid.Column>
-                        Information
+                            <Header as="h2">About Me</Header>
+                            I am a detailed oriented engineer with 1 year of experience in web development.
+                            I have a solid mathematical background to develop advanced and efficient algorithms and
+                            am very passionate about learning and using new emerging technologies. As a programmer I
+                            emphasize good code quality by following coding standards, Test-driven development, Pair
+                            programming, and code reviews. I am confident that my experience in the field qualifies me
+                            for consideration.
                     </Grid.Column>
-                </Grid.Row>
+
             </Grid>
         )
     }
